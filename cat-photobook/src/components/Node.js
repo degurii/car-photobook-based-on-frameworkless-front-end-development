be43f3f4333afe61a213341f093227dd1d03704d
degurii <sansam709@gmail.com>
2021-06-01 01:23:53 +0900

    Rename imgPath to iconPath

--- cat-photobook/src/components/Node.js
+++ cat-photobook/src/components/Node.js
@@ -1,12 +1,12 @@
 const Node = ({name, type}) => {
-    const imgPath = {
+    const iconPath = {
         FILE: './assets/file.png',
         DIRECTORY: './assets/directory.png',
         PREV: './assets/prev.png',
     }
     return `
     <div class="Node">
-        <img src="${imgPath[type]}" alt="${type} icon">
+        <img src="${iconPath[type]}" alt="${type} icon">
         ${type === 'PREV' ? '' : '<div>' + name + '</div>'}
     </div>
     `
