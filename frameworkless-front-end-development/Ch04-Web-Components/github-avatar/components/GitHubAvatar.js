45c0fe2a3c3ab935c2d32c1e9a1938113aa4b08e
degurii <sansam709@gmail.com>
2021-06-21 15:03:32 +0900

    Add custom events for request success, fail

--- frameworkless-front-end-development/Ch04-Web-Components/github-avatar/components/GitHubAvatar.js
+++ frameworkless-front-end-development/Ch04-Web-Components/github-avatar/components/GitHubAvatar.js
@@ -1,5 +1,12 @@
-const ERROR_IMAGE = 'https://files-82ee7vgzc.now.sh'
-const LOADING_IMAGE = 'https://files-8bga2nnt0.now.sh'
+import ERROR_IMAGE from 'url:./notification-error.png'
+import LOADING_IMAGE from 'url:./loading.png'
+const AVATAR_LOAD_COMPLETE = 'AVATAR_LOAD_COMPLETE';
+const AVATAR_LOAD_ERROR = 'AVATAR_LOAD_ERROR';
+
+export const EVENTS = {
+    AVATAR_LOAD_COMPLETE,
+    AVATAR_LOAD_ERROR,
+};
 
 const getGitHubAvatarUrl = async user => {
     if (!user) {
@@ -31,6 +38,24 @@ export default class GitHubAvatar extends HTMLElement {
         this.setAttribute('user', value);
     };
 
+    onLoadAvatarComplete() {
+        const event = new CustomEvent(AVATAR_LOAD_COMPLETE, {
+            detail: {
+                avatar: this.url,
+            },
+        });
+        dispatchEvent(event);
+    };
+
+    onLoadAvatarError(error) {
+        const event = new CustomEvent(AVATAR_LOAD_ERROR, {
+            detail: {
+                error,
+            },
+        });
+        dispatchEvent(event);
+    };
+
     render() {
         window.requestAnimationFrame(() => {
             this.innerHTML = '';
@@ -48,8 +73,10 @@ export default class GitHubAvatar extends HTMLElement {
 
         try {
             this.url = await getGitHubAvatarUrl(user);
+            this.onLoadAvatarComplete();
         } catch (e) {
             this.url = ERROR_IMAGE;
+            this.onLoadAvatarError(e);
         }
 
         this.render();
