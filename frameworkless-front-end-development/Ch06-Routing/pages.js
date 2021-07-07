fe37bf0472bc03b9b0704798027c0f8b798c9d17
degurii <sansam709@gmail.com>
2021-07-07 22:29:41 +0900

    Implement routing system based on fragment identifiers

---
+++ frameworkless-front-end-development/Ch06-Routing/pages.js
@@ -0,0 +1,17 @@
+export default container => {
+    const home = () => {
+        container.textContent = 'This is Home Page';
+    };
+    const list = () => {
+        container.textContent = 'This is List Page';
+    };
+    const notFound = () => {
+        conatiner.textContent = 'Page Not Found!';
+    };
+
+    return {
+        home,
+        list,
+        notFound,
+    };
+};
